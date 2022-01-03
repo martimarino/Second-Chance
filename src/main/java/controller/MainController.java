19f1974f22613092ae5eb3fc649e7669e5fb20e3
rmatrella <r.matrella@studenti.unipi.it>
2022-01-03 15:58:48 +0100

    Changes Home structure and inserting tabs controllers

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,19 @@
+package main.java.controller;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    private HomeController homeController;
+
+    @FXML
+    private MyProfileController myProfile;
+
+    @FXML
+    private SearchUserController searchUser;
+
+    @FXML
+    private SearchInsertionController searchInsertion;
+
+}
