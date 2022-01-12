23af1e4ca5273891ae49eb8d6418ca51e12e5744
rmatrella <92583548+rmatrella@users.noreply.github.com>
2022-01-12 18:51:28 +0100

    call of function to manage the insertion page

--- src/main/java/controller/InsertionController.java
+++ src/main/java/controller/InsertionController.java
@@ -32,9 +32,19 @@ public class InsertionController {
     public GridPane infoTable;
     public Label descriptionContainer;
     public Pane imgContainer;
+    public Text view;
+    public Text interested;
+    public Text timestamp;
+    public Text color;
+    public Text gender;
+    public Text size;
+    public Text status;
+    public Text category;
+    public Text brand;
+    public Text country;
+    public Text seller;
+    public Text price;
     String insertion_id;
-    Double price;
-    String seller;
     String image_url;
 
 
@@ -44,6 +54,9 @@ public class InsertionController {
         insertionTitle = new Label();
         infoContainer.setVisible(true);
 
+        Session session = Session.getInstance();
+        User user = session.getLoggedUser();
+
         ConnectionMongoDB conn = new ConnectionMongoDB();
         Insertion insertion = conn.findInsertion(insertion_id);
 
@@ -52,6 +65,17 @@ public class InsertionController {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        image_url = insertion.getImage_url();
+
+        ConnectionNeo4jDB connNeo = new ConnectionNeo4jDB();
+        String favouriteText;
+        if(!connNeo.showIfInterested(user.getUsername(), insertion_id))
+            favouriteText = "Add to favourite";
+        else
+            favouriteText = "Remove from favourite";
+
+        favourite.setText(favouriteText);
     }
 
 
@@ -65,65 +89,25 @@ public class InsertionController {
             images.setFitWidth(300);
             images.setImage(image);
             imgContainer.getChildren().add(images);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        price = insertion.getPrice();
-        seller = insertion.getSeller();
-        image_url = insertion.getImage_url();
+
         descriptionContainer.setText(insertion.getDescription());
 
-        Text price = new Text(insertion.getPrice() + " " + "€");
-        Text seller = new Text(insertion.getSeller());
-        Text country = new Text(insertion.getCountry());
-        Text status = new Text(insertion.getStatus());
-        Text color = new Text(insertion.getColor());
-        Text view = new Text(String.valueOf(insertion.getViews()));
-        Text interested = new Text(String.valueOf(insertion.getInterested()));
-        Text category = new Text(insertion.getCategory());
-        Text gender = new Text(insertion.getGender());
-        Text size = new Text(insertion.getSize());
-        Text timestamp = new Text(insertion.getTimestamp());
-        Text brand = new Text(insertion.getBrand());
-
-        Text seller_text = new Text("seller: ");
-        Text price_text = new Text("price: ");
-        Text country_text = new Text("country: ");
-        Text status_text = new Text("status: ");
-        Text color_text = new Text("color: ");
-        Text view_text = new Text("views: ");
-        Text interested_text = new Text("interested: ");
-        Text category_text = new Text("category: ");
-        Text gender_text = new Text("gender: ");
-        Text size_text = new Text("size: ");
-        Text timestamp_text = new Text("published: ");
-        Text brand_text = new Text("brand: ");
-
-        infoTable.add(price_text, 0, 0);
-        infoTable.add(price, 1, 0);
-        infoTable.add(seller_text, 0, 1);
-        infoTable.add(seller, 1, 1);
-        infoTable.add(country_text, 0, 2);
-        infoTable.add(country, 1, 2);
-        infoTable.add(brand_text, 0, 3);
-        infoTable.add(brand, 1, 3);
-        infoTable.add(category_text, 0, 4);
-        infoTable.add(category, 1, 4);
-        infoTable.add(status_text, 0, 5);
-        infoTable.add(status, 1, 5);
-        infoTable.add(size_text, 0, 6);
-        infoTable.add(size, 1, 6);
-        infoTable.add(gender_text, 0, 7);
-        infoTable.add(gender, 1, 7);
-        infoTable.add(color_text, 0, 8);
-        infoTable.add(color, 1, 8);
-        infoTable.add(timestamp_text, 0, 9);
-        infoTable.add(timestamp, 1, 9);
-        infoTable.add(view_text, 0, 10);
-        infoTable.add(view, 1, 10);
-        infoTable.add(interested_text, 0, 11);
-        infoTable.add(interested, 1, 11);
-
+        price.setText(insertion.getPrice() + " " + "€");
+        seller.setText(insertion.getSeller());
+        country.setText(insertion.getCountry());
+        status.setText(insertion.getStatus());
+        color.setText(insertion.getColor());
+        view.setText(String.valueOf(insertion.getViews()));
+        interested.setText(String.valueOf(insertion.getInterested()));
+        category.setText(insertion.getCategory());
+        gender.setText(insertion.getGender());
+        size.setText(insertion.getSize());
+        timestamp.setText(insertion.getTimestamp());
+        brand.setText(insertion.getBrand());
         
     }
 
@@ -132,7 +116,9 @@ public class InsertionController {
         ConnectionMongoDB conn = new ConnectionMongoDB();
         Session session = Session.getInstance();
         User user = session.getLoggedUser();
-        if(!conn.buyCurrentInsertion(insertion_id, user.getUsername(), price, seller, image_url))
+        String[] s = price.getText().split(" ");
+        Double insPrice = Double.valueOf(s[0]);
+        if(!conn.buyCurrentInsertion(insertion_id, user.getUsername(),insPrice , seller.getText(), image_url))
             Utility.infoBox("Cannot conclude the purchase, something wrong! /n Please retry later","Error", "Error buying product" );
         else
             Utility.infoBox("Product buyed correctly!" , "User Advise", "Purchase done" );
@@ -146,9 +132,20 @@ public class InsertionController {
         ConnectionNeo4jDB connNeo = new ConnectionNeo4jDB();
         ConnectionMongoDB connMongo = new ConnectionMongoDB();
 
-        connNeo.setFavouriteInsertion(user.getUsername(), insertion_id);
-        connMongo.updateNumInterested(insertion_id);
+        if(!connNeo.showIfInterested(user.getUsername(), insertion_id)) {
+            connNeo.setFavouriteInsertion(user.getUsername(), insertion_id);
+            connMongo.updateNumInterested(insertion_id, 1);
+            favourite.setText("Remove from favourite");
+            interested.setText(String.valueOf(Integer.parseInt(interested.getText()) +1));
+        }
+        else{
+            connNeo.dislikeInsertion(user.getUsername(), insertion_id);
+            connMongo.updateNumInterested(insertion_id, -1);
+            favourite.setText("Add to favourite");
+            interested.setText(String.valueOf(Integer.parseInt(interested.getText()) -1));
+        }
 
+        connNeo.showIfInterested(user.getUsername(), insertion_id);
     }
 
 
