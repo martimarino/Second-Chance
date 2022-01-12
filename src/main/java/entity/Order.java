bedef420a1569a6fb74393ec06824c824fdc3985
rmatrella <92583548+rmatrella@users.noreply.github.com>
2022-01-12 18:52:45 +0100

    order class

---
+++ src/main/java/entity/Order.java
@@ -0,0 +1,83 @@
+package main.java.entity;
+
+public class Order {
+
+    String order_id;
+    String seller;
+    String buyer;
+    String image;
+    double price;
+    String timestamp;
+
+
+    public Order(String order_id, String seller, String buyer, String image, double price, String timestamp){
+
+        this.order_id = order_id;
+        this.seller = seller;
+        this.buyer = buyer;
+        this.image = image;
+        this.price = price;
+        this.timestamp = timestamp;
+
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public String getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(String buyer) {
+        this.buyer = buyer;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "order_id='" + order_id + '\'' +
+                ", seller='" + seller + '\'' +
+                ", buyer='" + buyer + '\'' +
+                ", image='" + image + '\'' +
+                ", price=" + price +
+                ", timestamp='" + timestamp + '\'' +
+                '}';
+    }
+}
