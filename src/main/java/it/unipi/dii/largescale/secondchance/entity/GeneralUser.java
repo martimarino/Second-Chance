f8c18fecf8216eee507e9b284b6a2f0169405d2f
rmatrella <r.matrella@studenti.unipi.it>
2022-01-30 19:17:12 +0100

    try

---
+++ src/main/java/it/unipi/dii/largescale/secondchance/entity/GeneralUser.java
@@ -0,0 +1,14 @@
+package main.java.it.unipi.dii.largescale.secondchance.connection.entity;
+
+public interface GeneralUser {
+
+    public void setUsername(String username);
+
+    public void setPassword(String username);
+
+    public String getUsername();
+
+    public String getPassword();
+
+}
+
